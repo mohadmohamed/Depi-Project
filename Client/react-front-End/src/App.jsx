fa71633c505e8485b7357bf2c22f73f5b0342b31
Mohad Mohamed <mohadm756@gmail.com>
2025-08-30 05:38:13 +0300

    Sign Up page finished

--- Client/react-front-End/src/App.jsx
+++ Client/react-front-End/src/App.jsx
@@ -1,15 +1,16 @@
 
 import { useEffect, useState } from "react";
 import ResumeAnalysis from "./componenets/resume/ResumeAnalysis";
-
+import Login from "./componenets/resume/authintication/Login";  
+import Signup from "./componenets/resume/authintication/Signup";
 
 function App() {
-
-
 
   return (
     <>
       <ResumeAnalysis />
+      <Login />
+      <Signup />
     </>
   );
 }
