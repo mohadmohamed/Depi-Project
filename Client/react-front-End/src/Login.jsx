20ede85c56df2dce1e34d268bb235f2f4e0b4ecb
Mohad Mohamed <mohadm756@gmail.com>
2025-08-30 04:56:55 +0300

    Finished Login Page with styles

---
+++ Client/react-front-End/src/Login.jsx
@@ -0,0 +1,34 @@
+import './Login.css'
+
+export default function Login() {
+ 
+  return (
+    <main>
+       <h1>Welcome Back!</h1>
+       <p>Log In to Continue Your Job Journey</p>
+       <form action="">
+        <div className="card-outer">
+          <div className="card-inner">
+            <div>
+              <label htmlFor="email">Email Address</label>
+              <input type="email" name="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" placeholder="Enter your password" />
+            </div>
+
+            <button type="submit" id="submit">Log In</button>
+          </div>
+        </div>
+       </form>
+    <span>Don't have an account? <a href="">Sign Up</a></span>
+
+    
+    </main>
+   
+  )
+  
+ 
+}
+
