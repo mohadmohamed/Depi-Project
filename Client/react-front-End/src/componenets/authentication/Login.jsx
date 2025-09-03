52ef9a05f0b464e8f405283d51a01183343d2c6a
Mohad Mohamed <mohadm756@gmail.com>
2025-09-03 03:28:38 +0300

    added show and hide password

--- Client/react-front-End/src/componenets/authentication/Login.jsx
+++ Client/react-front-End/src/componenets/authentication/Login.jsx
@@ -1,8 +1,12 @@
 import './login.css'
+import React, { useState } from 'react';
 import Header from '../resume/Header'
 import Footer from '../resume/Footer'
 export default function Login() {
- 
+ const [showPassword, setShowPassword] = useState(false);
+ function toggleShowPassword() {
+  setShowPassword((prev) => !prev);
+}
   return (
     <>
     {/* <Header isLoggedIn={true}/> */}
@@ -18,7 +22,14 @@ export default function Login() {
             </div>
             <div>
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" placeholder="Enter your password" />
+              <input type={showPassword? 'text' : 'password'} name="password" id="password" placeholder="Enter your password" />
+                  <span className='pass-toggle-icon' onClick={toggleShowPassword} style={{
+                  color: showPassword ? '#312f2fff' : '#888888',
+                  fontSize: '0.78rem',
+                  textAlign: 'right',
+                  cursor: 'pointer',
+                  marginTop: '5px',
+                }}>Show password</span>
             </div>
 
             <button type="submit" id="submit">Log In</button>
