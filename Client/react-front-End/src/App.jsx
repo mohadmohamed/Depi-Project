f1d726c970f25940c12e6192ee3c26ac3f756d74
Mohad Mohamed <mohadm756@gmail.com>
2025-09-15 21:22:34 +0300

    Mock Interview Page Done

--- Client/react-front-End/src/App.jsx
+++ Client/react-front-End/src/App.jsx
@@ -4,11 +4,12 @@ import ResumeAnalysis from "./componenets/resume/ResumeAnalysis";
 import Login from "./componenets/authentication/Login";  
 import Signup from "./componenets/authentication/Signup";
 import Questions from "./componenets/Questions/Questions";
-
+import MockInterview from "./componenets/Mock-Interview/mockInterview";
 function App() {
 
   return (
     <>
+      <MockInterview />
       {/* <Questions /> */}
      {/* <Signup/> */}
       {/* <Login /> */}
