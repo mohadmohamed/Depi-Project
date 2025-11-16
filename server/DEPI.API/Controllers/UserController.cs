b686d224476ff9cf00300fe3c02d910302d309c3
mohamed abozed <M.masod2293@nu.edu.eg>
2025-11-17 00:49:59 +0200

    Edited somethings in the userService

--- server/DEPI.API/Controllers/UserController.cs
+++ server/DEPI.API/Controllers/UserController.cs
@@ -2,8 +2,10 @@
 using DEPI.Application.DTOs;
 using DEPI.Application.Services;
 using DEPI.DataAccess.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace DEPI.API.Controllers
 {
@@ -20,14 +22,29 @@ namespace DEPI.API.Controllers
         [HttpPost("Register")]
         public async Task<IActionResult> RegisterUser([FromBody]userRegisterDTO userDto)
         {
-            var user = await _userService.UserRegister(userDto);
-            return Ok("User Registerd");
+            // check that the email is a valid email 
+            bool isMail = userDto.Email.Contains("@");
+            if(isMail)
+            {
+               var user = await _userService.UserRegister(userDto);
+            }else
+            {
+                return BadRequest("email is wrong please reWrite it ");
+            }
+                return Ok("User Registerd");
         }
         [HttpPost("Login")]
         public async Task<IActionResult> LoginUser([FromBody] userLoginDTO userDto)
         {
             var token = await _userService.UserLogin(userDto);
             return Ok(new { token });
+        }
+        [Authorize]
+        [HttpGet("me")]
+        public IActionResult Me()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Ok(new { userId });
         }
 
     }
